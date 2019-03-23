d4da32ce3caf8a7ab4648e92583341a716352467
Pavel Svitek <svitek.pavel@gmail.com>
2019-03-23 10:42:37 +0100

    add viewport and charset to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ import styled, { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
@@ -19,10 +17,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Dancing+Script|EB+Garamond:400,500,600|Source+Sans+Pro"
             rel="stylesheet"
           />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta charSet="utf-8" />
 
           {this.props.styleTags}
         </Head>
